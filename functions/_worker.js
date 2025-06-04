b692c3889391e70dfde493504fd93885fdfe649b
davidshipp <49307646+davidshipp@users.noreply.github.com>
2025-06-04 10:41:32 +0100

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -3,6 +3,8 @@ export default {
     const response = await env.ASSETS.fetch(request);
     const newHeaders = new Headers(response.headers);
     newHeaders.set("Access-Control-Allow-Origin", "*");
+    newHeaders.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    newHeaders.set("Access-Control-Allow-Headers", "Content-Type");
     return new Response(response.body, {
       status: response.status,
       statusText: response.statusText,
