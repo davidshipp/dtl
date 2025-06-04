927cf0c33b0ba91ea78dd7ae10ce24aba903bf7e
davidshipp <49307646+davidshipp@users.noreply.github.com>
2025-06-04 10:49:21 +0100

    Update _worker.js

--- functions/_worker.js
+++ functions/_worker.js
@@ -1,10 +1,24 @@
 export default {
   async fetch(request, env, ctx) {
+    if (request.method === "OPTIONS") {
+      // Preflight response
+      return new Response(null, {
+        status: 204,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type",
+        },
+      });
+    }
+
+    // Normal GET/POST asset response
     const response = await env.ASSETS.fetch(request);
     const newHeaders = new Headers(response.headers);
     newHeaders.set("Access-Control-Allow-Origin", "*");
     newHeaders.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     newHeaders.set("Access-Control-Allow-Headers", "Content-Type");
+
     return new Response(response.body, {
       status: response.status,
       statusText: response.statusText,
